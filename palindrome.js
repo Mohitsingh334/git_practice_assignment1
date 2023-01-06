6d4934ac3c91978ad1ed6f18d6da471c4dc86b38
Mohit Singh <mohitsinghboy0@gmail.com>
2023-01-06 23:41:48 +0530

    added prime and palindrome problem

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+let str="naman";
+for(let i=str.length;i>0;i--){
+bag=str[i];
+}
+
+if(str=bag){
+console.log("Palindrome")
+}else{
+console.log("Palindomre);
---
+++ prime.js
@@ -0,0 +1,11 @@
+
+let count=0;
+for(let i=1;i,=num;i++){
+ if(num%i){
+count++
+}
+
+if(cpunt==1){
+console.log("prime)
+}else{
+console.log(Not prime);
